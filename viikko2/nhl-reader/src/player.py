42abac3acb0061eeab495087023a2de25c54967c
kaa <113045348+kaahy@users.noreply.github.com>
2023-11-13 17:32:50 +0200

    Tehtävä 10. Pelaajalista

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.team = dict['team']
     
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.team}) - goals: {self.goals} - assists: {self.assists}"
