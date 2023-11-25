4e3f902e10d3a3f596b959a3ad5e9cb8f2bc887c
kaa <113045348+kaahy@users.noreply.github.com>
2023-11-25 19:01:27 +0200

    Teht. 3. Tenniksen pisteenlaskun refaktorointi

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,40 @@
+SCORE_WORDS = ("Love", "Fifteen", "Thirty", "Forty")
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.points = {player1_name: 0, player2_name: 0}
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
-        else:
-            self.m_score2 = self.m_score2 + 1
+        self.points[player_name] += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        points1 = self.points[self.player1_name]
+        points2 = self.points[self.player2_name]
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if points1 == points2:
+            return self.tasapeli(points1)
+        elif points1 >= 4 or points2 >= 4:
+            return self.ylitetty_3(points1, points2)
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            return self.molemmilla_alle_4(points1, points2)
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+    def tasapeli(self, points):
+        if points in (0, 1, 2):
+            return SCORE_WORDS[points] + "-All"
+        return "Deuce"
+    
+    def ylitetty_3(self, points1, points2):
+        point_difference = points1 - points2
+        
+        if point_difference == 1:
+            return f"Advantage {self.player1_name}"
+        elif point_difference == -1:
+            return f"Advantage {self.player2_name}"
+        elif point_difference >= 2:
+            return f"Win for {self.player1_name}"
+        return f"Win for {self.player2_name}"
+    
+    def molemmilla_alle_4(self, points1, points2):
+        return SCORE_WORDS[points1] + "-" + SCORE_WORDS[points2]
